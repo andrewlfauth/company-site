e48758a0aee7fca864559d5ff5354d6d357252e1
andrewlfauth <andrewlfauth@gmail.com>
2022-09-24 14:28:59 -0700

    Added: sendClientFormData to HeroForm and Popup

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -3,18 +3,19 @@ import Services1 from '../components/index/Services1'
 import Services2 from '../components/index/Services2'
 import Popup from '../components/index/Popup'
 import CTASection from '../components/index/CTASection'
+import sendClientFormData from '../nodeMailer/sendClientFormData.server'
 
 export async function action({ request }) {
   // handle HeroForm && Popup
   const formData = await request.formData()
   const { ...values } = Object.fromEntries(formData)
 
-  await new Promise((res) => setTimeout(() => res(), 1000))
-
   if (values.component === 'HeroForm') {
+    sendClientFormData(values)
     return { HeroForm: { sent: true } }
   }
   if (values.component === 'Popup') {
+    sendClientFormData(values)
     return { Popup: { sent: true } }
   }
 
